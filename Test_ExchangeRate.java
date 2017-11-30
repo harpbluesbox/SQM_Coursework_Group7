332eea1c9679272c919f1c9b9707affe133d3355
Oliver Mitchell <harpbluesbox@gmail.com>
2017-11-30 11:32:48 +0000

    Updates Test file

--- Test_ExchangeRate.java
+++ Test_ExchangeRate.java
@@ -1,6 +1,11 @@
 package application;
 
 import org.testfx.framework.junit.ApplicationTest;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.testfx.*;
+import org.testfx.api.FxToolkit;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -21,6 +26,21 @@ public class Test_ExchangeRate extends ApplicationTest {
         stage.setScene(new Scene(mainNode));
         stage.show();
         stage.toFront();
+    }
+    
+    @Before
+    public void setUp () throws Exception {
+    }
+
+    @After
+    public void tearDown () throws Exception {
+      FxToolkit.hideStage();
+    }
+    
+    @Test
+    public void testEnglishInput () {
+      clickOn("#txt_Input");
+      write("This is a test!");
     }
     
     
