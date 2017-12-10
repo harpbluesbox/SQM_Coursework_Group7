9bdf0f635335ca5d8d6a4f889221c4f5db918045
Oliver Mitchell <harpbluesbox@gmail.com>
2017-12-10 14:37:21 +0000

    Commented

--- ControllerC.java
+++ ControllerC.java
@@ -25,47 +25,60 @@ public class ControllerC implements Initializable {
 	public void initialize(java.net.URL arg0, ResourceBundle arg1){
 		currencyList = generateCurrencyList();
 		populateComboBoxes();
+		
+		//adds a listener to the Investment Amount textbox so that invalid inputs are sanitised before they are used by the program
 		txt_InvestmentAmount.textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
 				// TODO Auto-generated method stub
-		        if(!arg2.matches("\\d*(\\.\\d{0,2})?")) {
+		        if(!arg2.matches("\\d*(\\.\\d{0,2})?")) //regex defining an integer or decimal with up to 2 decimal places
+		        {
 		            txt_InvestmentAmount.setText(arg1);
 		        }
 			}});
 			
+		//adds a listener to the APR textbox so that invalid inputs are sanitised before they are used by the program
 		txt_APR.textProperty().addListener(new ChangeListener<String>() {
 				@Override
 				public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
 					// TODO Auto-generated method stub
-			        if(!arg2.matches("\\d*(\\.\\d{0,4})?")) {
+			        if(!arg2.matches("\\d*(\\.\\d{0,4})?")) //regex defining an integer or decimal with up to 4 decimal places
+			        {
 			            txt_APR.setText(arg1);
 			        }
 				}
 		});
 		
+		//adds a listener to the years textbox so that invalid inputs are sanitised before they are used by the program
 		txt_Years.textProperty().addListener(new ChangeListener<String>() {
 			@Override
 			public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
 				// TODO Auto-generated method stub
-		        if(!arg2.matches("\\d*?")) {
+		        if(!arg2.matches("\\d*?")) //regex defining an integer
+		        {
 		            txt_Years.setText(arg1);
 		        }
 			}
 	});
 	}
 
+	//function that populates both the input and output currency exchange combo-boxes with the correct currnecy strings
 	public void populateComboBoxes() {
 		ObservableList<String> x = cbx_Input.getItems();
 		ObservableList<String> y = cbx_Output.getItems();
+		
+		//loops through list of currencies and adds them to comboboxes
 		for (int i = 0; i < 5; i++) {  
 			x.add(currencyList[i].getCurrencyName());
 			y.add(currencyList[i].getCurrencyName());
 		}
+		
+		//sets both comboboxes to first element by default
 		cbx_Input.setValue(cbx_Input.getItems().get(0));
 		cbx_Output.setValue(cbx_Output.getItems().get(0));
 	}
 	
+	//generate an array which contains all 5 currency objects
 	public Currency[] generateCurrencyList() {
 		Currency[] currencyList = new Currency[5];
 		currencyList[0] = new Currency("Australia(AUD)", new float[] {1.0f,0.97f,0.64f,0.58f, 0.76f});
@@ -76,85 +89,95 @@ public class ControllerC implements Initializable {
 		return currencyList;
 	}
 	
+	//function to calculate the exchange rate from one currency to another
 	private float CalculateExchangeRate(){
 		float result = -1.0f;
 		if(IsExchangeRateInputValid()){
 			float rate = 0.0f;
-			String inputCurrencyName = cbx_Input.getValue();
-			String outputCurrencyName = cbx_Output.getValue();
+			String inputCurrencyName = cbx_Input.getValue(); //get input currency
+			String outputCurrencyName = cbx_Output.getValue(); //get output currency
 			int fromIndex = GetIndexFromCurrencyName(inputCurrencyName);
 			int toIndex = GetIndexFromCurrencyName(outputCurrencyName);
 			
 			if(fromIndex != -1 && toIndex != -1){
-				rate = currencyList[fromIndex].getCurrencyRate()[toIndex];
+				rate = currencyList[fromIndex].getCurrencyRate()[toIndex]; //get the currency exchange rate multiplier
 			}
 			
-			result = Float.parseFloat(txt_Input.getText()) * rate;		
+			result = Float.parseFloat(txt_Input.getText()) * rate; //multiply the input amount by the exchange rate multiplier
 		}
-		return result;
+		return result; //return the new amount
 	}
 	
+	//function to calculate the interest forecast
 	private double CalculateInterest() {
 		double result = -1.0f;
 		if(IsInterestInputValid()) {
-			double investmentAmount = Double.parseDouble(txt_InvestmentAmount.getText());
-			double apr = Double.parseDouble(txt_APR.getText());
-			double years = Double.parseDouble(txt_Years.getText());
+			double investmentAmount = Double.parseDouble(txt_InvestmentAmount.getText()); //get the investment amount
+			double apr = Double.parseDouble(txt_APR.getText()); //get the APR
+			double years = Double.parseDouble(txt_Years.getText()); //get the number of years of the investment
 			result = investmentAmount;
 			for(int i = 0; i < years; i++){
-				result += ((result/100)*apr);
+				result += ((result/100)*apr); //loop through the number of years, and times by the APR each year
 			}
 		}
-		return result;
+		return result; //return the new amount
 	}
 	
+	//function to test whether the exchange rate amount input is valid
 	private boolean IsExchangeRateInputValid(){
 		
 		boolean result;
 		
-		String regex_OneDecimalPointOnly = "(\\d+(?:\\.\\d+)?)";
-		String val = txt_Input.getText();
+		String regex_OneDecimalPointOnly = "(\\d+(?:\\.\\d+)?)"; //regex for an integer or a decimal with one decimal point
+		String val = txt_Input.getText(); //get the input amount
 		
-		result = val.matches(regex_OneDecimalPointOnly);
+		result = val.matches(regex_OneDecimalPointOnly); //boolean value defining whether input is valid
 		
 		return result;
 	}
 	
+	//function to test whether the interest inputs are valid
 	private boolean IsInterestInputValid(){
 		
 		boolean isOnlyNumbers = false;
-		String regex_OneDecimalPointOnly = "(\\d+(?:\\.\\d+)?)";
-		String regex_IntegersOnly = "([0-9]+)";
-		String val1 = txt_InvestmentAmount.getText();
-		String val2 = txt_APR.getText();
-		String val3 = txt_Years.getText();	
+		String regex_OneDecimalPointOnly = "(\\d+(?:\\.\\d+)?)"; //regex for an integer or a decimal with one decimal point
+		String regex_IntegersOnly = "([0-9]+)"; //regex for an integer
+		String val1 = txt_InvestmentAmount.getText(); //get investment amount
+		String val2 = txt_APR.getText(); //get APR
+		String val3 = txt_Years.getText(); //get number of years
+		
+		//boolean value showing if the inputs are all correct numbers
 		isOnlyNumbers = val1.matches(regex_OneDecimalPointOnly) && val2.matches(regex_OneDecimalPointOnly) && val3.matches(regex_IntegersOnly);
 		
 		if(isOnlyNumbers)
 		{
-		boolean isLessThan11Digits = false;
+			
+		//test whether inputs is less than 9 digits long
+			boolean isLessThan9Digits = false;
 			if(Double.parseDouble(val1) < 100000000.0f)
 			{
-				isLessThan11Digits = true;
-				System.out.println("Parsed value " + Double.parseDouble(val1));
+				isLessThan9Digits = true;
 			}
 			
+		//test that APR is 50% or below
 			boolean aprIs50OrLower = false;
 			if(Double.parseDouble(val2) <= 50.0f)
 			{
 				aprIs50OrLower = true;
 			}		
 			
-			boolean lessthan20years = false;
+		//test that number of years is less than or equal to 20
+			boolean lessthanorequalto20years = false;
 			if(Integer.parseInt(val3) <= 20)
 			{
-				lessthan20years = true;
+				lessthanorequalto20years = true;
 			}
-			return isLessThan11Digits && aprIs50OrLower && lessthan20years && isOnlyNumbers;
+			return isLessThan9Digits && aprIs50OrLower && lessthanorequalto20years && isOnlyNumbers;
 		}
 		return false;
 	}
 	
+	//gets the index of the correct currency in the combobox by its string
 	private int GetIndexFromCurrencyName(String currencyName){
 		int result = -1;
 		switch(currencyName)
@@ -178,65 +201,73 @@ public class ControllerC implements Initializable {
 		return result;
 	}
 	
+	//event handler fired when the calculate exchange rate button is clicked
 	public void btn_CalculateExchangeRate_OnClick(){
 		if(!txt_Input.getText().isEmpty()) {
-			float result = CalculateExchangeRate();
+			float result = CalculateExchangeRate(); //calculate the exchange rate
 			if(result != -1.0f){
-				String resultTo2DecimalPlaces = String.format("%.2f", result);
+				String resultTo2DecimalPlaces = String.format("%.2f", result); //format the result to 2 decimal places
 				SetExchangeRateOutput(resultTo2DecimalPlaces);
+				
+				//print out the result
 				SetExchangeRateInfo(
 						txt_Input.getText() + " " + cbx_Input.getValue()
 						+ " equals " + txt_Output.getText()
 						+ " " + cbx_Output.getValue() + ".");
 			}
 			else {
+				//if there is an error, show an error message
 				SetExchangeRateOutput("");
 				SetExchangeRateInfo("An error occured! Input should be a real number only.");
 			}
 		}
 	}
 	
+	//event handler fired when the calculate interest button is clicked
 	public void btn_CalculateInterest_OnClick() {
 		if(!txt_InvestmentAmount.getText().isEmpty()
 				&& !txt_APR.getText().isEmpty()
 				&& !txt_Years.getText().isEmpty()) {
-			double result = CalculateInterest();
+			double result = CalculateInterest(); //calculate the interest
 			if(result != -1.0f) {
-				String resultTo2DecimalPlaces = String.format("%.2f",  result);
+				String resultTo2DecimalPlaces = String.format("%.2f",  result); //format the result to 2 decimal places
 				SetFutureValue(resultTo2DecimalPlaces);
 				SetInterestInfo("");
 			}
 			else {
+				//if there is an error show an error message.
 				SetFutureValue("");
 				SetInterestInfo("An error occurred! Input should be a real number only. \n Investment Amount should be less than 10,000,000,000 \n APR should be <= 50% \n Years should be an integer <= 20");
 			}
 		}
 		else {
+			//if there is an error show an error message.
 			SetFutureValue("");
 			SetInterestInfo("An error occurred! Input should be a real number only. \n Investment Amount should be less than 10,000,000,000 \n APR should be <= 50% \n Years should be an integer <= 20");
 		}
 	}
 	
+	//sets the output of the investment forecast
 	private void SetFutureValue(String text) {
 		txt_FutureValue.setText(text);
 	}
 	
+	//sets the text for the message box for the investment forecast
 	private void SetInterestInfo(String text) {
 		lbl_InterestInfo.setText(text);
 	}
 	
+	//sets the text for the message box for the exchange rate calculator
 	private void SetExchangeRateInfo(String text) {
 		lbl_ExchangeRateInfo.setText(text);
 	}
 	
+	//sets the output of the exchange rate calculator
 	private void SetExchangeRateOutput(String text) {
 		txt_Output.setText(text);
 	}
 	
-	private void SetExchangeRateInput(String text) {
-		txt_Input.setText(text);
-	}
-	
+	//class to hold currency name and currency exchange rates
 	public class Currency{
 		private String currencyName;
 		private float[] currencyRate;
